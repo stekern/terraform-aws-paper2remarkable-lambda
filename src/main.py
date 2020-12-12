0b58c930ec14fe06375c3252876293392e736c18
stekern <stekern@users.noreply.github.com>
2020-12-13 00:03:44 +0100

    Use subprocess to run paper2remarkable

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ import json
 import os
 import sys
 import logging
-from paper2remarkable import ui
+import subprocess
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -36,19 +36,17 @@ def lambda_handler(event, context):
 
     # Does not seem like paper2remarkable is set up as a Python library, so we pass in
     # parameters by simulating that we're calling it from the command-line
-    sys.argv = [
+    args = [
         *(["--verbose"] if payload.get("verbose", True) else []),
         *(["--blank"] if payload.get("blank", False) else []),
         *(["--center"] if payload.get("center", False) else []),
         *(["--right"] if payload.get("right", False) else []),
         *(["--no-crop"] if payload.get("crop", True) else []),
-        *(["--filename", payload["filename"]] if payload.get("filename", None) else []),
         *(
             ["--remarkable-path", payload["remarkable_path"]]
             if payload.get("remarkable_path", None)
             else []
         ),
-        *(payload["inputs"] if len(payload.get("inputs", [])) else []),
     ]
 
     credentials_file = "/tmp/rmapi.conf"
@@ -62,20 +60,24 @@ devicetoken: {rmapi_config['rmapi_device_token']}
     logger.debug("Stored rmapi credentials in '%s'", credentials_file)
     os.environ["RMAPI_CONFIG"] = credentials_file
 
-    try:
-        logger.debug("Running paper2remarkable")
+    successes = []
+    for file in payload["inputs"]:
         # We get some weird errors at times:
         # `[Errno 2] No such file or directory`
         # that seem to stem from inability to start a shell:
         # `shell-init: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory`
         # This is most likely due to reuse of Lambda execution contexts,
         # as the error does not seem to occur on clean executions.
-        # Changing the directory to a known directory seems to fix it.
+        # Only processing a single input at a time and changing the
+        # directory to a known directory seems to fix it.
+        logger.debug("Running paper2remarkable for file '%s'", file)
         os.chdir("/tmp")
-        ui.main()
-        status_code = 200
-    except:
-        logger.error("Failed to run paper2remarkable")
-        logger.exception()
-        status_code = 500
-    return {"statusCode": status_code}
+        out = subprocess.run(["p2r"] + args + [file], capture_output=True)
+        logger.debug("paper2remarkable output %s", out)
+        if out.returncode == 0:
+            successes.append(file)
+        else:
+            logger.warn("paper2remarkable command had non-zero exit")
+    status_code = 200 if len(successes) == len(payload["inputs"]) else 500
+    response = {"statusCode": status_code}
+    return response
